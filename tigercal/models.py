94301163612d3fd97fc39293cb525fd7c4143a70
Matthew Fastow <matthewfastow@gmail.com>
2020-02-24 18:47:33 -0500

    added db settings

--- tigercal/models.py
+++ tigercal/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+	username = models.CharField(max_length=100)
+
+class Group(models.Model):
+	name = models.CharField(max_length=100)
+	description = models.TextField()
+
+class Membership(models.Model):
+	student = models.ForeignKey(Student, on_delete=models.CASCADE)
+	group = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+class Leadership(models.Model):
+	student = models.ForeignKey(Student, on_delete=models.CASCADE)
+	group = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+class Event(models.Model):
+	group = models.ForeignKey(Group, on_delete=models.CASCADE)
+	name = models.CharField(max_length=200)
+	start_time = models.DateTimeField()
+	end_time = models.DateTimeField()
+	descrip = models.TextField()
