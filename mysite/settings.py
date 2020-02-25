3d7c846e091544bf66a925925f80e3108eaebf3e
Matthew Fastow <matthewfastow@gmail.com>
2020-02-24 20:27:42 -0500

    hid secret key

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd$!s%yb*h9mjt$hle5^(9sa3@5ylyvq(l&*^u8sscbpuu735rc'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
