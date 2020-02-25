b2e7a49c2adcafee1a570dfdb5b84b0aa70fb5c6
Matthew Fastow <matthewfastow@gmail.com>
2020-02-24 21:25:34 -0500

    added admin db permissions

--- tigercal/admin.py
+++ tigercal/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Student)
+admin.site.register(Group)
+admin.site.register(Membership)
+admin.site.register(Leadership)
+admin.site.register(Event)
